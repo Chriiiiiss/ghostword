52e64b86cc33f0aa24b1b8a7fb45028dc787b4a9
Chris <chris.fraanc@gmail.com>
2023-04-20 08:59:56 +0200

    [ADD] Add relation between data value and cloud color on WordCloudComponent

--- src/components/WordCloudComponent.jsx
+++ src/components/WordCloudComponent.jsx
@@ -2,8 +2,32 @@ import React from "react";
 import WordCloud from "react-wordcloud";
 
 const WordCloudComponent = (props) => {
-  console.log(props.wordData);
   const { wordData } = props;
+  const minValue = Math.min(...wordData.map((word) => word.value));
+  const maxValue = Math.max(...wordData.map((word) => word.value));
+
+  const getColor = (value, min, max) => {
+    const lowColor = "#B3C2F9";
+    const mediumColor = "#7D98F8";
+    const highColor = "#597BF7";
+
+    const factor = (value - min) / (max - min);
+
+    if (factor < 0.5) {
+      return interpolateColor(
+        hexToRgb(lowColor),
+        hexToRgb(mediumColor),
+        factor * 2
+      );
+    } else {
+      return interpolateColor(
+        hexToRgb(mediumColor),
+        hexToRgb(highColor),
+        (factor - 0.5) * 2
+      );
+    }
+  };
+
   const options = {
     colors: ["#B3C2F9", "#7D98F8", "#597BF7"],
     rotations: 2,
@@ -12,6 +36,7 @@ const WordCloudComponent = (props) => {
     fontStyle: "normal",
     fontWeight: "normal",
     padding: 5,
+    color: (word) => getColor(word.value, minValue, maxValue),
   };
 
   return <WordCloud words={props.wordData} options={options} />;
