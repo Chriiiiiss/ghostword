0d7c9a462e0a9e3f0746de0f5d94ffef25379f10
Chris <chris.fraanc@gmail.com>
2023-04-20 09:11:03 +0200

    [MOD] Remove relation between data value and cloud color on WordCloudComponent (not working lol)

--- src/components/WordCloudComponent.jsx
+++ src/components/WordCloudComponent.jsx
@@ -3,30 +3,6 @@ import WordCloud from "react-wordcloud";
 
 const WordCloudComponent = (props) => {
   const { wordData } = props;
-  const minValue = Math.min(...wordData.map((word) => word.value));
-  const maxValue = Math.max(...wordData.map((word) => word.value));
-
-  const getColor = (value, min, max) => {
-    const lowColor = "#B3C2F9";
-    const mediumColor = "#7D98F8";
-    const highColor = "#597BF7";
-
-    const factor = (value - min) / (max - min);
-
-    if (factor < 0.5) {
-      return interpolateColor(
-        hexToRgb(lowColor),
-        hexToRgb(mediumColor),
-        factor * 2
-      );
-    } else {
-      return interpolateColor(
-        hexToRgb(mediumColor),
-        hexToRgb(highColor),
-        (factor - 0.5) * 2
-      );
-    }
-  };
 
   const options = {
     colors: ["#B3C2F9", "#7D98F8", "#597BF7"],
@@ -36,7 +12,8 @@ const WordCloudComponent = (props) => {
     fontStyle: "normal",
     fontWeight: "normal",
     padding: 5,
-    color: (word) => getColor(word.value, minValue, maxValue),
+    getRandomColor: (word, idx, size) =>
+      getColor(word.value, minValue, maxValue),
   };
 
   return <WordCloud words={props.wordData} options={options} />;
